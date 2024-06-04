5e62f2d5337a74c06df83808eb26603a59a992be
sugiaryfiantoro <sugiaryfiantoro@gmail.com>
2024-06-04 20:43:50 +0700

    pertemuan 10

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -69,6 +69,46 @@ public function laporan_buku_pdf(){
       'buku'=>$this->ModelBuku->getBuku()->result_array());
       $this->load->view('buku/export_excel_buku',$data);
   }
+  public function laporan_pinjam(){
+    $data['judul'] = 'Laporan Data Peminjaman';
+    $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+    $data['laporan'] =$this->db->query("select*from pinjam p,detail_pinjam d, buku b, user u where d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array();
   
+    $this->load->view('templates/header', $data);
+    $this->load->view('templates/sidebar', $data);
+    $this->load->view('templates/topbar', $data);
+    $this->load->view('pinjam/laporan-pinjam', $data);
+    $this->load->view('templates/footer');
+  }
+  public function cetak_laporan_pinjam(){
+    $data['laporan'] =$this->db->query("select*from pinjam p,detail_pinjam d, buku b, user u where d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array();
+  
+    $this->load->view('pinjam/laporan-print-pinjam',$data);
+  }
+  public function laporan_pinjam_pdf(){
+    $data['laporan'] =$this->db->query("select*from pinjam p,detail_pinjam d, buku b, user u where d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array();
+  
+      //script untuk dompdf php versi 5
+      $this->load->library('dompdf_gen');
+      $this->load->view('pinjam/laporan-pdf-pinjam', $data);
+  
+      $paper_size  = 'A4'; // ukuran kertas
+      $orientation = 'landscape'; //tipe format kertas potrait atau landscape
+      $html = $this->output->get_output();
+  
+      $this->dompdf->set_paper($paper_size, $orientation);
+      // Convert to PDF
+      $this->dompdf->load_html($html);
+      $this->dompdf->render();
+      $this->dompdf->stream("laporan data peminjaman.pdf", array('Attachment' => 0));
+      // nama file pdf yang di hasilkan
+  }
+  public function export_excel_pinjam(){
+    $data=array(
+      'title'=>'Laporan Data Peminjaman Buku',
+      'laporan'=>$this->db->query("select*from pinjam p,detail_pinjam d, buku b, user u where d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array());
+      $this->load->view('pinjam/export-excel-pinjam',$data);
+  }
+    
   
 }
